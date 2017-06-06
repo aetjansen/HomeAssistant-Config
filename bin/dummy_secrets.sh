9985d7ec93ecd0a148dccbc33b5ad9acb6e8be8a
Arno <arno@jansenpoolman.nl>
2017-06-06 23:45:27 +0200

    Trying to fix broken dummy secrets for asuswrt

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -44,7 +44,7 @@ echo "asus_router_ip: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "asus_router_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "asus_router_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "asus_router_port: 1234" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "asus_router_key_path: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "asus_router_key_path: /home/dummy/mykey.pub" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "pushover_api_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "pushover_user_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "zone1_name: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
